333af63d4c81d1a7714dce3f99667f56dc1669e5
kingshuk441 <kingshuk441@gmail.com>
2024-07-02 16:19:10 +0530

    03 async unary

--- src/test/java/com/example/test/sec06/AbstractTest.java
+++ src/test/java/com/example/test/sec06/AbstractTest.java
@@ -10,11 +10,13 @@ import org.junit.jupiter.api.BeforeAll;
 public class AbstractTest extends AbstractChannelTest {
     private final GrpcServer grpcServer = GrpcServer.create(new BankService());
     protected BankServiceGrpc.BankServiceBlockingStub blockingStub;
+    protected BankServiceGrpc.BankServiceStub stub;
 
     @BeforeAll
     public void setup() {
         this.grpcServer.start();
         this.blockingStub = BankServiceGrpc.newBlockingStub(this.channel);
+        this.stub = BankServiceGrpc.newStub(this.channel);
     }
 
     @AfterAll
---
+++ src/test/java/com/example/test/sec06/L02UnaryAsyncClientTest.java
@@ -0,0 +1,44 @@
+package com.example.test.sec06;
+
+import com.example.models.sec06.AccountBalance;
+import com.example.models.sec06.BalanceCheckRequest;
+import com.google.protobuf.Empty;
+import io.grpc.stub.StreamObserver;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.CountDownLatch;
+
+public class L02UnaryAsyncClientTest extends AbstractTest {
+    private static final Logger log = LoggerFactory.getLogger(L02UnaryAsyncClientTest.class);
+
+    @Test
+    public void getBalanceAsyncTest() throws InterruptedException {
+        var request = BalanceCheckRequest.newBuilder()
+                .setAccountNumber(1).build();
+        var latch = new CountDownLatch(1);
+        this.stub.getAccountBalance(request, new StreamObserver<AccountBalance>() {
+            @Override
+            public void onNext(AccountBalance accountBalance) {
+                log.info("account balance async {}", accountBalance);
+                Assertions.assertEquals(100, accountBalance.getBalance());
+                latch.countDown();
+            }
+
+            @Override
+            public void onError(Throwable throwable) {
+
+            }
+
+            @Override
+            public void onCompleted() {
+
+            }
+        });
+        latch.await();
+
+    }
+
+}

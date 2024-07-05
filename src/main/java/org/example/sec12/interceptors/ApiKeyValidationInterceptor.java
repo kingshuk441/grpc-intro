bfb32c76ab61b4cd478a9e7a34b3499c894c0fc1
kingshuk441 <kingshuk441@gmail.com>
2024-07-05 12:06:07 +0530

    12 rpc info in server interceptor

--- src/main/java/org/example/sec12/interceptors/ApiKeyValidationInterceptor.java
+++ src/main/java/org/example/sec12/interceptors/ApiKeyValidationInterceptor.java
@@ -1,15 +1,20 @@
 package org.example.sec12.interceptors;
 
 import io.grpc.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Objects;
 
 import static org.example.sec12.Constants.API_KEY;
 
 public class ApiKeyValidationInterceptor implements ServerInterceptor {
+    private static final Logger log = LoggerFactory.getLogger(ApiKeyValidationInterceptor.class);
+
     @Override
     public <ReqT, RespT> ServerCall.Listener<ReqT> interceptCall(ServerCall<ReqT, RespT> serverCall, Metadata metadata, ServerCallHandler<ReqT, RespT> serverCallHandler) {
         var apiKey = metadata.get(API_KEY);
+        log.info("proto package name: {}",serverCall.getMethodDescriptor().getFullMethodName());
         if (isValid(apiKey)) {
             return serverCallHandler.startCall(serverCall, metadata);
         }
